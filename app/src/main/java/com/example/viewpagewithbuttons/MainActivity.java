590310637bdd0a9e6394b9b427681d6310851cd3
RNTAdmin <raghuladhitya2013@gmail.com>
2019-06-14 18:32:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/viewpagewithbuttons/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.viewpagewithbuttons;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends FragmentActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/viewpagewithbuttons/MoviesFragment.java
@@ -0,0 +1,20 @@
+package com.example.viewpagewithbuttons;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MoviesFragment extends Fragment {
+    View mView;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.movies_fragment,container,false);
+        return mView;
+    }
+}
